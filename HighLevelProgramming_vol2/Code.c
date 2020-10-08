71df77a5286460cf8ffa73f8a5291a56c1bde474
DerFreimensch <k.d.markov@outlook.com>
2020-10-08 14:12:24 +0300

    Добавьте файлы проекта.

---
+++ HighLevelProgramming_vol2/Code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define N 5
+
+int main(void) {
+	int mas[N];
+	int* pt = mas;
+	int ch1, ch2;
+	int Flag = 0;
+	for (int i = 0; i < N; i++) {
+		scanf_s("%d", &mas[i]);
+	}
+	for (int j = N-1; j > 2; j--) {
+		ch1 = *(pt+j) / *(pt + j - 1);
+		ch2 = *(pt + j - 1) / *(pt + j - 2);
+		if (ch1 != ch2) {
+			Flag = 0;
+			printf("%d ", Flag);
+			exit();
+		}
+		
+	}
+	printf("%d ", ch1);
+	
+	return 0;
+}
